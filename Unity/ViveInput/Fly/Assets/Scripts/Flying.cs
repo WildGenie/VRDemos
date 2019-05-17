b8b1e1e35d98e3e69ff8450e1da8e98a8df07f0d
VRLab <papr0003@stud.hs-kl.de>
2019-05-17 16:09:14 +0200

    implemented flying with ViveInput

---
+++ Unity/ViveInput/Fly/Assets/Scripts/Flying.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class Flying : MonoBehaviour {
+    
+    public GameObject head;
+    public GameObject rightHand;
+    public GameObject leftHand;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ViveInput.GetPress(HandRole.RightHand, ControllerButton.FullTrigger) || ViveInput.GetPress(HandRole.LeftHand, ControllerButton.FullTrigger)) flying();
+	}
+
+    private void flying()
+    {
+        Vector3 direction = (rightHand.transform.position - head.transform.position) / speed;        
+        transform.position += direction;
+    }
+}
