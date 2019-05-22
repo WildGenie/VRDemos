4e7b87b121536d7dfc149ed187a8c387fa14fa2e
MBrill <manfred.brill@hs-kl.de>
2019-05-22 20:34:48 +0200

    HelloWorld, Meshes und FirstInteraction mit Vive Input realisiert.

---
+++ Unity/viveInput/helloViveInput/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Erstes Beispiel einer C# Klasse in einer Unity-Anwendung
+/// </summary>
+public class QuitApplication : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Vive Input verwendet ESC, um die Simulation zu stoppen.
+        // Deshalb verwenden wir hier die Taste Q wie "Quit".
+        // Eventuell ist es möglich, Q nur auszuwerten, wenn bereits
+        // ESC betätigt ist. Frage also: können wir den Status des
+        // Simulators abfragen?
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            Application.Quit();
+            // Esc is ignored in Editor playback mode
+            #if UNITY_EDITOR
+           UnityEditor.EditorApplication.isPlaying = false;
+           #endif
+        }
+    }
+
+
+}
