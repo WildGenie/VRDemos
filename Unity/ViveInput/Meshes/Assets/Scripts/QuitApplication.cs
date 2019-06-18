d63a4f748b2605718669d7ec65a4518bc1bdaffd
brill <manfred.brill@hs-kl.de>
2019-06-18 13:42:08 +0200

    Wieder ESC, q ist in wasd im Simulator ...

--- Unity/ViveInput/Meshes/Assets/Scripts/QuitApplication.cs
+++ Unity/ViveInput/Meshes/Assets/Scripts/QuitApplication.cs
@@ -3,23 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Erstes Beispiel einer C# Klasse in einer Unity-Anwendung
+///Anwendung mit Esc-Taste beenden
 /// </summary>
 public class QuitApplication : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-        // Vive Input verwendet ESC, um die Simulation zu stoppen.
-        // Deshalb verwenden wir hier die Taste Q wie "Quit".
-        // Eventuell ist es möglich, Q nur auszuwerten, wenn bereits
-        // ESC betätigt ist. Frage also: können wir den Status des
-        // Simulators abfragen?
-        if (Input.GetKeyUp(KeyCode.Q))
+	void Update ()
+    {
+        if (Input.GetKeyUp(key: KeyCode.Escape))
         {
             Application.Quit();
             // Esc is ignored in Editor playback mode
